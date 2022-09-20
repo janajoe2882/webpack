a40494a12a1062f89e6c34362d7ce20828d56b24
Chosy <wyswid@gmail.com>
2022-09-20 20:57:39 +0900

    change img size

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ import WebImg from "./wimg.png";
 window.addEventListener("DOMContentLoaded", () => {
   const el = document.querySelector("#app");
   el.innerHTML = `<h1>1+2=${sum(1, 2)}</h1>
-  <img src=${WebImg} style="width:20%" />
+  <img src=${WebImg} style="width:30%" />
   `;
 });
