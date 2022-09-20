54c9b2a60a44fee73e19508191e257d58fa7827b
Chosy <wyswid@gmail.com>
2022-09-20 20:54:01 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import { sum } from "./math";
+import "./app.css";
+import WebImg from "./wimg.png";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const el = document.querySelector("#app");
+  el.innerHTML = `<h1>1+2=${sum(1, 2)}</h1>
+  <img src=${WebImg} style="width:20%" />
+  `;
+});
